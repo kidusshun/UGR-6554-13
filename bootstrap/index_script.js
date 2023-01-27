4dbeef1fca4657f99fcdf17795f7b868684ba556
unknown <kidusshun@gmail.com>
2023-01-27 21:15:11 +0300

    bootstrap

---
+++ bootstrap/index_script.js
@@ -0,0 +1,24 @@
+var firstBox=document.getElementById("first");
+var secondBox=document.getElementById("second");
+var thirdBox=document.getElementById("third");
+
+function firstBar(){
+    firstBox.style.left="150px";
+    secondBox.style.left="2000px";
+    thirdBox.style.left="3000px";
+}
+
+
+function secondBar(){
+    firstBox.style.left="-2000px";
+    secondBox.style.left="150px";
+    thirdBox.style.left="2000px";
+}
+
+
+
+function thirdBar(){
+    firstBox.style.left="-3000px";
+    secondBox.style.left="-2000px";
+    thirdBox.style.left="150px";
+}
---
+++ bootstrap/login-button.js
@@ -0,0 +1,7 @@
+function loginalert(){
+    alert("You are now logged in");
+}
+
+function signupalert(){
+    alert("welcome to learnit");
+}
